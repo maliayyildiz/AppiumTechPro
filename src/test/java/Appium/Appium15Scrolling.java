018e9ee1c7a7cb946b1161504610d37792ea4638
maliayyildiz <69386343+maliayyildiz@users.noreply.github.com>
2022-09-14 20:06:08 +0200

    scroll

---
+++ src/test/java/Appium/Appium15Scrolling.java
@@ -0,0 +1,45 @@
+package Appium;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
+import org.testng.annotations.Test;
+
+
+import java.net.MalformedURLException;
+
+public class Appium15Scrolling extends BaseClass{
+
+    @Test
+    public void test() throws MalformedURLException, InterruptedException {
+        AndroidDriver driver = getAndroidDriver();
+
+        System.out.println("App intstalled...");
+        //api demos butonuna tikla
+        driver.findElementByXPath("//android.widget.TextView[@text='API Demos']").click();
+        Thread.sleep(3000);
+//preference butonuna tikladik
+        driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+        //preference dependencies butonuna bastik
+        Thread.sleep(5000);
+
+        Dimension dimension = driver.manage().window().getSize();
+
+        int start_x = (int) (dimension.width * 0.5);
+        int start_y = (int) (dimension.height * 0.8);
+
+        int end_x = (int) (dimension.width * 0.5);
+        int end_y = (int) (dimension.height * 0.2);
+
+        TouchAction touchAction = new TouchAction(driver);
+
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(3000);
+            touchAction.press(PointOption.point(start_x,start_y))
+                    .moveTo(PointOption.point(end_x,end_y)).release().perform();
+        }
+
+
+    }
+}
