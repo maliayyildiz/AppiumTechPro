e03f18831bf3e739ef1838759b5b9965a1206476
maliayyildiz <69386343+maliayyildiz@users.noreply.github.com>
2022-09-14 20:46:36 +0200

    uiScrollable

--- src/test/java/Appium/Appium15Scrolling.java
+++ src/test/java/Appium/Appium15Scrolling.java
@@ -24,21 +24,26 @@ public class Appium15Scrolling extends BaseClass{
         //preference dependencies butonuna bastik
         Thread.sleep(5000);
 
-        Dimension dimension = driver.manage().window().getSize();
+        //uiScrollable
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Switches\"))");
 
-        int start_x = (int) (dimension.width * 0.5);
-        int start_y = (int) (dimension.height * 0.8);
-
-        int end_x = (int) (dimension.width * 0.5);
-        int end_y = (int) (dimension.height * 0.2);
-
-        TouchAction touchAction = new TouchAction(driver);
-
-        for (int i = 0; i < 3; i++) {
-            Thread.sleep(3000);
-            touchAction.press(PointOption.point(start_x,start_y))
-                    .moveTo(PointOption.point(end_x,end_y)).release().perform();
-        }
+        driver.findElementByAndroidUIAutomator("UiSelector().className(\"android.widget.TextView\").text(\"Switches\")").click();
+//
+//        Dimension dimension = driver.manage().window().getSize();
+//
+//        int start_x = (int) (dimension.width * 0.5);
+//        int start_y = (int) (dimension.height * 0.8);
+//
+//        int end_x = (int) (dimension.width * 0.5);
+//        int end_y = (int) (dimension.height * 0.2);
+//
+//        TouchAction touchAction = new TouchAction(driver);
+//
+//        for (int i = 0; i < 3; i++) {
+//            Thread.sleep(3000);
+//            touchAction.press(PointOption.point(start_x,start_y))
+//                    .moveTo(PointOption.point(end_x,end_y)).release().perform();
+//        }
 
 
     }
