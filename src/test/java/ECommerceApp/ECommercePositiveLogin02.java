cc26a1469847b7073d48d70a381e57eca99369f5
maliayyildiz <69386343+maliayyildiz@users.noreply.github.com>
2022-09-20 22:24:56 +0200

    ecommrce

---
+++ src/test/java/ECommerceApp/ECommercePositiveLogin02.java
@@ -0,0 +1,69 @@
+package ECommerceApp;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class ECommercePositiveLogin extends BaseECommerceApp{
+    /*
+    //1- Fill the form details and verify Toast error messages displayed appropriately for wrong inputs
+    //1- hatali data ile form doldurdugunuzda hata mesajini dogrulayin
+//2-Shop the items in the app by scrolling to specific Product and add to cart
+//3-Validate if the items selected in the page 2 are matching with the items displayed in check out page
+//4- Validate the total Amount displayed in the checkout page matches with sum of product amounts selected for shopping
+//5-Validate Mobile gestures working for link (long press) and navigate to WebView //6-Verify if user can do operations on Web view and navigate back to native app if needed.
+(go to google and search “appium” then navigate to NATIVE APP and verify it)
+
+     */
+
+
+    @Test
+    public void positiveTest() throws MalformedURLException, InterruptedException {
+        AndroidDriver<MobileElement> driver = getAndroidDriver();
+        Thread.sleep(7000);
+        //1- Fill the form details and verify Toast error messages displayed appropriately for wrong inputs
+        //1- hatali data ile form doldurdugunuzda hata mesajini dogrulayin
+
+        MobileElement homeScreenTitle = driver.findElementById("toolbar_title");
+        MobileElement countrySpinner = driver.findElementById("spinnerCountry");
+
+
+        MobileElement nameBox = driver.findElementById("com.androidsample.generalstore:id/nameField");
+        MobileElement maleRadioButton = driver.findElementById("com.androidsample.generalstore:id/radioMale");
+        MobileElement femaleRadioButton = driver.findElementById("com.androidsample.generalstore:id/radioFemale");
+        MobileElement letsShopButton = driver.findElementById("com.androidsample.generalstore:id/btnLetsShop");
+
+//ana sayfa da miyiz? onayladik
+
+        //ulkeyi secmek icin ulke butununa bastik
+        countrySpinner.click();
+        //ulkeyi sececegiz
+        Thread.sleep(4000);
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Turkey\"))");
+        MobileElement expectedCountry = driver.findElementByXPath("//android.widget.TextView[@text='Turkey']");
+        expectedCountry.click();
+        Thread.sleep(5000);
+        //istedigimiz ulkeyi dogru olarak sectik mi?
+        MobileElement selectedCountry = driver.findElementByXPath("//android.widget.TextView[@text='Turkey']");
+        System.out.println(selectedCountry.getText());
+        Assert.assertEquals(selectedCountry.getText(), "Turkey");
+        Thread.sleep(3000);
+//negative case icin name bos olmali
+//        Assert.assertTrue(nameBox.getText().isEmpty());
+        nameBox.sendKeys("Ali");
+        //female secilsin
+        maleRadioButton.click();
+        //shop butonuna tiklayalim
+        letsShopButton.click();
+//hata mesajini onayla
+        Thread.sleep(2000);
+
+
+        //close app
+        driver.closeApp();
+
+    }
+}
